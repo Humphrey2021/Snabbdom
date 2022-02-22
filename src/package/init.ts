05333128cb4e3c4d1ffb3f678f133dfd4aeccda0
Humphrey <humphrey2021@163.com>
2022-02-23 01:59:36 +0800

    push

--- src/package/init.ts
+++ src/package/init.ts
@@ -86,6 +86,7 @@ export function init (modules: Array<Partial<Module>>, domApi?: DOMAPI) {
   }
 
   function createElm (vnode: VNode, insertedVnodeQueue: VNodeQueue): Node {
+    // 执行用户设置的 init 钩子函数
     let i: any
     let data = vnode.data
     if (data !== undefined) {
@@ -97,12 +98,16 @@ export function init (modules: Array<Partial<Module>>, domApi?: DOMAPI) {
     }
     const children = vnode.children
     const sel = vnode.sel
+    // 把 vnode 转换成真实 DOM 对象（没有渲染到页面）
     if (sel === '!') {
+      // 如果选择器为 ！，创建注释节点
       if (isUndef(vnode.text)) {
         vnode.text = ''
       }
       vnode.elm = api.createComment(vnode.text!)
     } else if (sel !== undefined) {
+      // 如果选择器不为空
+      // 解析选择器
       // Parse selector
       const hashIdx = sel.indexOf('#')
       const dotIdx = sel.indexOf('.', hashIdx)
@@ -115,6 +120,7 @@ export function init (modules: Array<Partial<Module>>, domApi?: DOMAPI) {
       if (hash < dot) elm.setAttribute('id', sel.slice(hash + 1, dot))
       if (dotIdx > 0) elm.setAttribute('class', sel.slice(dot + 1).replace(/\./g, ' '))
       for (i = 0; i < cbs.create.length; ++i) cbs.create[i](emptyNode, vnode)
+      // 如果 vnode 中有子节点，创建子 vnode 对应的 DOM 元素并追加到 DOM 树上
       if (is.array(children)) {
         for (i = 0; i < children.length; ++i) {
           const ch = children[i]
@@ -133,8 +139,10 @@ export function init (modules: Array<Partial<Module>>, domApi?: DOMAPI) {
         }
       }
     } else {
+      // 如果选择器为空，创建文本节点
       vnode.elm = api.createTextNode(vnode.text!)
     }
+    // 返回新创建的 DOM
     return vnode.elm
   }
 
